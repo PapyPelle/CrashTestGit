c7df1530fd8fa56bd17f58dd9b87674313208494
Yann <yroyant@enseirb-matmeca.fr>
2020-05-28 15:31:43 -0400

    solving with sat lib ; display grid output ; changing some public declaration

--- ex2_sudoku/src/stev/booleans/examples/SudokuBooleanChecker.java
+++ ex2_sudoku/src/stev/booleans/examples/SudokuBooleanChecker.java
@@ -1,7 +1,11 @@
 package stev.booleans.examples;
 
+import org.sat4j.core.VecInt;
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.IProblem;
+import org.sat4j.specs.ISolver;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -17,23 +21,42 @@ public class SudokuBooleanChecker {
 	static final int length = 3;
 	static final int lengthSqr = length * length;
 	
-	public PropositionalVariable varList[][][];
-	public BooleanFormula sudokuFormula = null;
+	// All propositional variables you need for sudoku (lengthSqr^3)
+	private PropositionalVariable varList[][][];
+	// The general Sudoku formula representing the rules
+	private BooleanFormula sudokuFormula = null;
+	// List of formula for the boxes already filled
+	private List<BooleanFormula> filledVariables = null;
+	// One solution of the Sudoku if it exist, relative to filledVariables
+	private int[] solution = null;
 	
+	/**
+	 * Create a Sudoku solver with the general rules and variables
+	 */
 	public SudokuBooleanChecker() {
+		// Create all variables
 		varList =  new PropositionalVariable[lengthSqr][lengthSqr][lengthSqr];
 		for(int i = 0; i < lengthSqr; ++i) {
 			for(int j = 0; j < lengthSqr; ++j) {
 				for(int k = 0; k < lengthSqr; ++k) {
-					String name = "r" + (i+1) + "c" + (j+1) + "d" + (k+1);
+					String name = getVariableName(i,j,k);
 					varList[i][j][k] = new PropositionalVariable(name);
-					System.out.println(name);
 				}
 			}
 		}
-		
+		// Create the general sudoku formula
 		sudokuFormula = new And(formula1(), formula2(), formula3(), formula4());
 	}
+	
+	
+	/**
+	 * Method for string variable conversion
+	 * @return the name of the literal 
+	 */
+	private String getVariableName(int row, int column, int number) {
+		return "r" + (row+1) + "c" + (column+1) + "d" + (number+1);
+	}
+	
 	
 	/**
 	 * Return the Xor of the operands, it is evaluated to true iff only one operand is true
@@ -59,10 +82,11 @@ public class SudokuBooleanChecker {
 		return new And(exclusions);
 	}
 	
+	
 	/**
 	 * Generate the formula corresponding to having only one digit per cell
 	 */
-	public BooleanFormula formula1() {
+	private BooleanFormula formula1() {
 		List<BooleanFormula> results = new ArrayList<BooleanFormula>();
 		List<BooleanFormula> consideredVariables = new ArrayList<BooleanFormula>();
 		for(int i = 0; i < lengthSqr; ++i) {
@@ -80,7 +104,7 @@ public class SudokuBooleanChecker {
 	/**
 	 * Generate the formula corresponding to having each digit on each line
 	 */
-	public BooleanFormula formula2() {
+	private BooleanFormula formula2() {
 		List<BooleanFormula> results = new ArrayList<BooleanFormula>();
 		List<BooleanFormula> consideredVariables = new ArrayList<BooleanFormula>();
 		//for each line
@@ -101,7 +125,7 @@ public class SudokuBooleanChecker {
 	/**
 	 * Generate the formula corresponding to having each digit on each column
 	 */
-	public BooleanFormula formula3() {
+	private BooleanFormula formula3() {
 		List<BooleanFormula> results = new ArrayList<BooleanFormula>();
 		List<BooleanFormula> consideredVariables = new ArrayList<BooleanFormula>();
 		//for each column
@@ -122,7 +146,7 @@ public class SudokuBooleanChecker {
 	/**
 	 * Generate the formula corresponding to having each digit on each square
 	 */
-	public BooleanFormula formula4() {
+	private BooleanFormula formula4() {
 		List<BooleanFormula> results = new ArrayList<BooleanFormula>();
 		List<BooleanFormula> consideredVariables = new ArrayList<BooleanFormula>();
 		//for each square
@@ -145,46 +169,132 @@ public class SudokuBooleanChecker {
 	}
 	
 	/**
-	 * args of format xxxxxxxxxxxxxxxxxxxxxxxxxx where each x is either 1 to 9 or #, lengthSqr ^ 2 x
+	 * Parse the inputData and adapt it for the Sudoku checker
+	 * @param inputData
 	 */
-	public static void main(String[] args) 
-	{
-		String input = args[0];
-		System.out.println(input);
-		SudokuBooleanChecker sbc = new SudokuBooleanChecker();
-		
-		List<BooleanFormula> consideredVariables = new ArrayList<BooleanFormula>();
-		for(int id = 0; id < input.length(); ++id) {
-			char c = input.charAt(id);
+	public void addVariables(String inputData) {
+		filledVariables = new ArrayList<BooleanFormula>();
+		for(int id = 0; id < inputData.length(); ++id) {
+			char c = inputData.charAt(id);
 			if(c == '#')
 				continue;
 			else {
-				int k = Character.getNumericValue(c) - 1;
 				int i = id / lengthSqr;
 				int j = id % lengthSqr;
-				consideredVariables.add(new Or(sbc.varList[i][j][k]));
-				System.out.println(id + ": " + c + ", [" + i + ", " + j + ", " + k + "]");
+				int k = Character.getNumericValue(c) - 1;
+				filledVariables.add(varList[i][j][k]);
+				// System.out.println(id + ": " + c + ", [" + i + ", " + j + ", " + k + "]");
 			}
 		}
-		consideredVariables.add(sbc.sudokuFormula);
-		
-		BooleanFormula big_formula = new And(consideredVariables);
+	}
+	
+	/**
+	 * Method used to resolve a Sudoku grid considering a set of known variables
+	 * @return true if satisfiable, also fill the solution attribute
+	 */
+	public boolean solveSudoku() throws Exception {
+		// If no data, do nothing
+		if (filledVariables == null) {
+			System.out.println("No data filled : please use the addVariables method before calling this one, else it's always satisfiable.");
+			return true;
+		};
+		// Add the sudoku rules into the one-literal clause data
+		filledVariables.add(sudokuFormula);
+		// Create the big formula
+		BooleanFormula big_formula = new And(filledVariables);
 		// Convert this formula to CNF
 		BooleanFormula cnf = big_formula.toCnf();
+		// Get solver specifications
+		int numVar = lengthSqr*lengthSqr*lengthSqr;
+		int clauses[][] = cnf.getClauses();
+		int numClauses = clauses.length;
+		// Create and initialize solver
+		ISolver solver = SolverFactory.newDefault();
+		solver.newVar(numVar);
+		solver.setExpectedNumberOfClauses(numClauses);
+		// Add clauses (integers arrays) one by one
+		for (int i=0; i < numClauses; i++) {
+			solver.addClause(new VecInt(clauses[i]));
+		}
+		// Use problem instance to get results
+		IProblem problem = solver;
+		if(problem.isSatisfiable()) {
+			Map<String,Integer> varMap = cnf.getVariablesMap();
+			solution = new int[lengthSqr*lengthSqr];
+			// Extract model and map back the correct values
+			int[] model = problem.findModel();
+			for(int i=0; i < lengthSqr; i++) {
+				for(int j=0; j < lengthSqr; j++) {
+					for(int k=0; k < lengthSqr; k++) {
+						String var = getVariableName(i,j,k);
+						int varId = varMap.get(var) - 1;
+						if (model[varId] > 0) {
+							solution[i*lengthSqr + j] = k+1;
+						}
+					}
+				}
+			}
+			return true;
+		}
+		// else
+		solution = null;
+		return false;
+	}
+	
+	/**
+	 * Allow to reuse the class with another set of inputs
+	 */
+	public void resetData() {
+		solution = null;
+		filledVariables = null;
+	}
+	
+	/**
+	 * Print sudoku grid solution (if there is one)
+	 */
+	public void printSolution() {
+		if (solution == null) {
+			System.out.println("No Solution.");
+		} else {
+			for(int i=0; i < solution.length; i++) {
+				if (i != 0 && i%lengthSqr == 0)
+					System.out.println("|");
+				if (i%(lengthSqr*length) == 0)
+					System.out.println("-------------------------");
+				if (i%length == 0)
+					System.out.print("| ");
+				System.out.print(solution[i]+ " ");
+			}
+			System.out.println("|");
+			System.out.println("-------------------------");
+		}
+	}
+	
+	
+	/**
+	 * Check the Sudoku passed as argument and print one result if it's solvable
+	 * @param args Format xxxxxxxxxx where each x is either 1 to 9 or #; lengthSqr ^ 2 x
+	 */
+	public static void main(String[] args) 
+	{
+		String input = "#################################################################################";
+		if (args.length > 0) {
+			input = args[0];
+		}
+		else {
+			// input = "#26###81#3##7#8##64###5###7#5#1#7#9###39#51###4#3#2#5#1###3###25##2#4##9#38###46#";
+		}
+		System.out.println("input : " + input);
 		
-		// Let's print it again
-		System.out.println(cnf);
-		
-		// Export this formula as an array of clauses
-		int[][] clauses = cnf.getClauses();
-		
-		// What's in that array? First element corresponds to first clause: [1, -2, 3]
-		System.out.println(Arrays.toString(clauses[0]));
-		// Second element corresponds to second clause: [1, -3, 1, 2]
-		System.out.println(Arrays.toString(clauses[1]));
-		
-		// What is the integer associated to variable q?
-		Map<String,Integer> associations = cnf.getVariablesMap();
-		System.out.println("Variable 999 is associated to number " + associations.get("r9c9d9"));
+		SudokuBooleanChecker sbc = new SudokuBooleanChecker();
+		sbc.addVariables(input);
+		try {
+			sbc.solveSudoku();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		sbc.printSolution();
+		return;
 	}
 }
